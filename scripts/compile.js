0ddef4f8f7b1aee1af2973460bd43690b56a6ec1
Saahil <saahilattud@gmail.com>
2024-05-01 03:49:40 +0000

    Script Changes

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,2 +1,31 @@
 // runs on nodev20 (fetch built in)
 // todo
+const fs = require('fs')
+const path = require('path')
+
+const files = fs.readdirSync('links')
+let all = {
+    '_NOTE': `Automated compile`,
+    'lastCompileDate': Date.now(),
+    links: {}
+}
+// todo make asyc so it works
+files.forEach((f) => {
+    console.log(`[*] Running ${f}.`)
+    const ff_path = path.join(__dirname, 'links', f)
+    const ff = fs.readdirSync(ff_path)
+const fullPath = path.join(ff_path, ff.find(e=>e.endsWith('.json') && !e.includes('.meta.')))
+const metadata = require(path.join(ff_path, 'metadata.json'))
+if(metadata.download_url) {
+    console.log(`[*] Downloading...`)
+    fetch(download_url).then(r=>r.text()).then(text => {
+        let json = metadata.format == 'txt' ? text.split('\n') : JSON.parse(text)
+        // if(metadata.format == 'txt') json = 
+        fs.writeFileSync(fullPath, JSON.stringify(json, null, 2))
+        console.log(`[*] Downloaded.`)
+    })
+} else {
+    console.log(`[*] Skipping, no download URL`)
+}
+})
+fs.writeFileSync(path.join(__dirname, '..', 'all.json'), JSON.stringify(all, null, 2))
--- scripts/just_add_them.js
+++ scripts/just_add_them.js
@@ -1,22 +1,21 @@
 const fs = require('fs')
 const path = require('path')
 
-const files = fs.readdirSync('links')
+const files = fs.readdirSync('../links')
+let all = {
+    '_NOTE': `Automated compile`,
+    'lastCompileDate': Date.now(),
+    links: {}
+}
 files.forEach((f) => {
     console.log(`[*] Running ${f}.`)
-    const ff_path = path.join(__dirname, 'links', f)
+    const ff_path = path.join(__dirname,'..', 'links', f)
     const ff = fs.readdirSync(ff_path)
-const fullPath = path.join(ff_path, ff.find(e=>e.endsWith('.json') && !e.includes('.meta.')))
-const metadata = require(path.join(ff_path, 'metadata.json'))
-if(metadata.download_url) {
-    console.log(`[*] Downloading...`)
-    fetch(download_url).then(r=>r.text()).then(text => {
-        let json = metadata.format == 'txt' ? text.split('\n') : JSON.parse(text)
-        // if(metadata.format == 'txt') json = 
-        fs.writeFileSync(fullPath, JSON.stringify(json, null, 2))
-        console.log(`[*] Downloaded.`)
-    })
-} else {
-    console.log(`[*] Skipping, no download URL`)
-}
+const fullPath = path.join(ff_path, ff.find(e=>e.endsWith('.json') && !e.includes('metadata.json')))
+// const metadata = require(path.join(ff_path, 'metadata.json'))
+const pathName = path.join(f,  ff.find(e=>e.endsWith('.json') && !e.includes('metadata.json')))
+console.log(pathName)
+all.links[pathName] = require(fullPath)
 })
+fs.writeFileSync(path.join(__dirname, '..', 'all.json'), JSON.stringify(all, null, 2))
+
